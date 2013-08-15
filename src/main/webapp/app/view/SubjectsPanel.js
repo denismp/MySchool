7cfdabe2eab99a8e3888eebec922ea20933258ec
Denis Putnam <denisputnam@verizon.net>
2013-08-14 20:48:34 -0400

    Put back

--- src/main/webapp/app/view/SubjectsPanel.js
+++ src/main/webapp/app/view/SubjectsPanel.js
@@ -82,7 +82,7 @@ Ext.define('MySchool.view.SubjectsPanel', {
                     xtype: 'tool',
                     handler: function(event, toolEl, owner, tool) {
                         window.console.log( "Save..." );
-                        debugger;
+                        //debugger;
                         var pnl = this.up('subjectspanel').down('subjectsgridpanel');
                         var mystore = pnl.getStore();
                         var myProxy = mystore.getProxy();
@@ -90,9 +90,9 @@ Ext.define('MySchool.view.SubjectsPanel', {
                         var myid = tool.id;
                         var myeventtype = event.type;
                         //tool.fireEvent( event.type, mystore, myProxy, mymodel, myid );
-                        mystore.save();
+                        //mymodel.save();
                         //mystore.commitChanges();
-                        //mystore.sync();
+                        mystore.sync();
 
                     },
                     id: 'mytoolsavesubjects',
