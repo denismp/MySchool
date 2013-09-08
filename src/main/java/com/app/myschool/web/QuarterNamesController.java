572d5be8e2c15211bde38d089c956b32c534ef2b
Denis Putnam <denisputnam@verizon.net>
2013-09-08 09:15:45 -0400

    web mvc json all

--- src/main/java/com/app/myschool/web/QuarterNamesController.java
+++ src/main/java/com/app/myschool/web/QuarterNamesController.java
@@ -1,6 +1,7 @@
 package com.app.myschool.web;
 
 import com.app.myschool.model.QuarterNames;
+import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,5 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/quarternameses")
 @Controller
 @RooWebScaffold(path = "quarternameses", formBackingObject = QuarterNames.class)
+@RooWebJson(jsonObject = QuarterNames.class)
 public class QuarterNamesController {
 }
