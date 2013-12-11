9255360746c3435b5e07567cb0b871970d7869f2
Don Hautsch <don@hautsch.com>
2013-12-11 18:58:46 -0500

    Deleted QuarterNames tests

--- src/test/java/com/app/myschool/model/QuarterNamesDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.app.myschool.model;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = QuarterNames.class)
-public class QuarterNamesDataOnDemand {
-}
