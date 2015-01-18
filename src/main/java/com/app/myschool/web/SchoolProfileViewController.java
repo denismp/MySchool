f2f22ee3ae26e7701243add0a4d502c0e898f84c
Denis Putnam <denisputnam@verizon.net>
2015-01-18 11:47:40 -0500

    Trying to fix School.  It isn't seen by the app.

--- src/main/java/com/app/myschool/web/SchoolProfileViewController.java
+++ src/main/java/com/app/myschool/web/SchoolProfileViewController.java
@@ -1,24 +1,21 @@
 package com.app.myschool.web;
 import com.app.myschool.model.School;
-import java.util.List;
-import java.util.Map;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 
+
 @RequestMapping("/schoolprofileviews")
 @Controller
-@RooWebScaffold(path = "schools", formBackingObject = School.class)
+
 @RooWebJson(jsonObject = School.class)
 public class SchoolProfileViewController {
 
