e5a1007d1133429aed5c4b0a67b8ecf7973ca184
Denis Putnam <denisputnam@verizon.net>
2015-02-17 15:15:36 -0500

    Hopefully moved things to 4.2.3.  Trying to fix tooltips.

--- src/main/webapp/build/temp/production/MySchool/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,3 +0,0 @@
-// @tag full-page
-// @require /Users/denisputnam/git/MySchool/src/main/webapp/MySchool/ext/ext-theme-classic.js
-// @require /Users/denisputnam/git/MySchool/src/main/webapp/MySchool/app.js
